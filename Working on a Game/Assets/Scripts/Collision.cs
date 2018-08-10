1bdcf278665fbb536c83bd4700de3917c6250c60
CreeperzonePlayz <37713924+CreeperzonePlayz@users.noreply.github.com>
2018-08-10 19:02:44 -0400

    HUGE changes made basis of game
    made walls, movement, collision, and the end of level 01

---
+++ Working on a Game/Assets/Scripts/Collision.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour {//Start
+
+    //Public 
+    public PlayerMovement playermove;
+    public MeshRenderer playermesh;
+
+
+    //Private
+
+
+
+    //Start of voids
+	private void OnCollisionEnter(UnityEngine.Collision collisionInfo)
+	{
+        if (collisionInfo.collider.tag == "Wall")
+        {
+            playermove.enabled = false;
+        }
+        if (collisionInfo.collider.tag == "WinLevel")
+        {
+            playermove.enabled = false;
+            playermesh.enabled = false;
+            Debug.Log("Win!");
+        }
+	}
+    //void start only checks once game is started
+	private void Start()
+	{
+		
+	}
+    //Void update checks every frame
+	private void Update()
+	{
+		
+	}
+
+}//End
